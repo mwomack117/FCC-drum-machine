4ef1d305cafb3bb3b19f5c36d9eb0c3517167d24
mwomack <michael.womack84@gmail.com>
2020-05-05 11:38:09 -0700

    Updates

---
+++ precache-manifest.831abb65950c93ef79177e8fe493b979.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99973bcfe359a546288efcbdb40100e",
+    "url": "/FCC-drum-machine/index.html"
+  },
+  {
+    "revision": "6bee776442c401d98947",
+    "url": "/FCC-drum-machine/static/css/main.66eacf3a.chunk.css"
+  },
+  {
+    "revision": "916d85d8d72a793e3cc6",
+    "url": "/FCC-drum-machine/static/js/2.32996f47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-drum-machine/static/js/2.32996f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bee776442c401d98947",
+    "url": "/FCC-drum-machine/static/js/main.512d2ec0.chunk.js"
+  },
+  {
+    "revision": "166095dee470f6e2b21e",
+    "url": "/FCC-drum-machine/static/js/runtime-main.107a70bc.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/FCC-drum-machine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/FCC-drum-machine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/FCC-drum-machine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/FCC-drum-machine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/FCC-drum-machine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/FCC-drum-machine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/FCC-drum-machine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/FCC-drum-machine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/FCC-drum-machine/static/media/tom.572ed39d.wav"
+  }
+]);
